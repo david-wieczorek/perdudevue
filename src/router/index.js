4a31c368bedf4c7f9e359df121b669890078215e
David Wieczorek <blackratio@gmail.com>
2017-09-06 20:54:10 +0200

    adding component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import Test from '@/components/Test';
+import UserProfile from '@/components/Profil';
 
 Vue.use(Router);
 
@@ -10,6 +12,19 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
+      children: [
+        {
+          // `UserProfile` va être rendu à l'intérieur du `<router-view>` de `User`
+          // quand `/utilisateur/:id/profil` concorde
+          path: 'profile',
+          component: UserProfile,
+        },
+      ],
     },
   ],
 });
